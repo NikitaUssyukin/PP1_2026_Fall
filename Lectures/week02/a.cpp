eb618175fa968438b51d772f70253a98aa3e8b2d
NikitaUssyukin <nikus0108@gmail.com>
2026-09-12 15:52:42 +0500

    upd

---
+++ Lectures/week02/a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // one-line comment
+
+    string message;
+    cin >> message;
+    cout << message << endl;
+
+    /*
+    multi-line
+    comment
+    */
+
+    return 0;
+}
